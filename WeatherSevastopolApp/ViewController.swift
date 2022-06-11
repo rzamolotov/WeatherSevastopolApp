4341bf84593efb5d86bddde2d2c3e70632807f01
Roman <roman@imac-roman.local>
2022-06-11 12:38:00 +0300

    Initial Commit

---
+++ WeatherSevastopolApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherSevastopolApp
+//
+//  Created by Roman on 11.06.2022.
+//  Copyright © 2022 Roman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
